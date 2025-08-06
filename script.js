0a83315b9d51f3a60e7a41f65463d07b82419c6a
Hammad-Niazi <hammadniazi991@gmail.com>
2025-08-07 00:58:24 +0200

    add animation effect on accordion wuestions and write a comment for to remove all active elements in js file first

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ questions.forEach((question)=>{
         questions.forEach((q)=>{
             q.classList.remove("active");
             q.nextElementSibling.style.display = "none";
-        })
+        }) // First closes all the active questions
         if(!isActive){
             question.classList.add("active");
             question.nextElementSibling.style.display = "block";
