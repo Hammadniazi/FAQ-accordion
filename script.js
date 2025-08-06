27e56493395e3f6deee84ceb12252b64b1ca41fe
Hammad-Niazi <hammadniazi991@gmail.com>
2025-08-06 21:00:04 +0200

    create FAQs accordion project in html,css and javascript

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const questions = document.querySelectorAll('.accordion-question');
+const questions = document.querySelectorAll(".accordion-question");
 
-questions.forEach((question) => {
-  question.addEventListener('click', () => {
-    const isActive = question.classList.contains('active');
 
-    // Close all open items
-    questions.forEach((q) => {
-      q.classList.remove('active');
-      q.nextElementSibling.style.display = 'none';
-    });
-
-    // If it wasn't already open, open this one
-    if (!isActive) {
-      question.classList.add('active');
-      question.nextElementSibling.style.display = 'block';
-    }
-  });
-});
+questions.forEach((question)=>{
+    question.addEventListener(("click"), ()=>{
+        const isActive = question.classList.contains("active")
+        
+        questions.forEach((q)=>{
+            q.classList.remove("active");
+            q.nextElementSibling.style.display = "none";
+        })
+        if(!isActive){
+            question.classList.add("active");
+            question.nextElementSibling.style.display = "block";
+        }
+    })
+})
